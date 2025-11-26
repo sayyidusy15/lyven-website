239643ab35326b59b25fb41b94fd5b5653ffa73d
Sayyidusy SA <sijeding15@gmail.com>
2025-11-26 21:43:57 +0700

    main home
    merubah yang awalnya home2 menjadi main home

--- src/app/(home1)/layout.tsx
+++ src/app/(home1)/layout.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import Header1 from '../Components/Header/Header1';
-import Footer1 from '../Components/Footer/Footer1';
-
+import React from "react";
+import Header1 from "../Components/Header/Header1";
+import Footer1 from "../Components/Footer/Footer1";
+import Header2 from "../Components/Header/Header2";
 
 const DefalultLayout = ({ children }) => {
-    return (
-        <div className='main-page-area'>
-            <Header1></Header1>
-            {children}
-            <Footer1></Footer1>
-        </div>
-    );
+  return (
+    <div className="main-page-area3">
+      <Header2></Header2>
+      {children}
+      {/* <Footer2></Footer2> */}
+    </div>
+  );
 };
 
 export default DefalultLayout;
--- src/app/(home1)/page.tsx
+++ src/app/(home1)/page.tsx
@@ -1,36 +1,45 @@
 import React from 'react';
-import HeroBanner1 from '../Components/HeroBanner/HeroBanner1';
+import Brand2 from '../Components/Brand/Brand2';
+import HowWeDo2 from '../Components/HowWeDo/HowWeDo2';
+import Services2 from '../Components/Services/Services2';
+import Project2 from '../Components/Project/Project2';
+import Pricing1 from '../Components/Pricing/Pricing1';
+import Awards from '../Components/Award/Awards';
+import Testimonial2 from '../Components/Testimonial/Testimonial2';
+import Faq1 from '../Components/Faq/Faq1';
+import Contact2 from '../Components/Contact/Contact2';
+import Blog2 from '../Components/Blog/Blog2';
+import HeroBanner4 from '../Components/HeroBanner/HeroBanner4';
 import About1 from '../Components/About/About1';
-import Counter1 from '../Components/Counter/Counter1';
-import Services1 from '../Components/Services/Services1';
-import Project1 from '../Components/Project/Project1';
-import Marquee from '../Components/Marquee/Marquee';
 import HowWeDo from '../Components/HowWeDo/HowWeDo';
-import Process1 from '../Components/Process/Process1';
-import Video1 from '../Components/Video/Video1';
-import Brand1 from '../Components/Brand/Brand1';
+import PortfolioShowcase from '../Components/Project/PortfolioShowcase';
 import Testimonial1 from '../Components/Testimonial/Testimonial1';
-import Team1 from '../Components/Team/Team1';
-import Contact1 from '../Components/Contact/Contact1';
-import Blog1 from '../Components/Blog/Blog1';
+import Pricing2 from '../Components/Pricing/Pricing2';
+import ServiceCategory from '../Components/Services/ServiceCategory';
+import Marquee2 from '../Components/Marquee/Marquee2';
 
 const page = () => {
     return (
         <div>
-            <HeroBanner1></HeroBanner1>
-            <About1></About1>
-            <Counter1></Counter1>
-            <Services1></Services1>
-            <Project1></Project1>
-            <Marquee></Marquee>
+            {/* <HeroBanner1></HeroBanner1> */}
+            <HeroBanner4></HeroBanner4>
+            <Brand2></Brand2>
             <HowWeDo></HowWeDo>
-            <Process1></Process1>
-            <Video1></Video1>
-            <Brand1></Brand1>
+            <PortfolioShowcase></PortfolioShowcase>
+            {/* <Testimonial1></Testimonial1> */}
+            <ServiceCategory></ServiceCategory>
+            {/* <Marquee2></Marquee2> */}
+            <Pricing2></Pricing2>
+            <Faq1></Faq1>
+            {/* <HowWeDo2></HowWeDo2>
+            <Services2></Services2>
+            <Project2></Project2>
+            <Pricing1></Pricing1>
+            <Awards></Awards>
             <Testimonial1></Testimonial1>
-            <Team1></Team1>
-            <Contact1></Contact1>
-            <Blog1></Blog1>
+            
+            <Contact2></Contact2>
+            <Blog2></Blog2> */}
         </div>
     );
 };
